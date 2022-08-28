5a05681e8077a3cf9bb78765548ee393a7940bc6
guard scripts <legendarygeorgeward@gmail.com>
2022-08-28 03:22:05 -0700

    Initial Commit

--- load.js
+++ load.js
@@ -1,9 +1,21 @@
+var list = document.querySelector('#list')
+var a
+var url
+var name
 fetch('./data.json')
   .then((response) => response.json())
   .then((data) => {
     // console.log(data)
     
     data.forEach(element => {
-        
+        a = document.createElement('a');
+        url = element.url
+        buttonName = element.name
+        a.classList.add("game")
+        if (element.hosted) { url = url.replace('%s',`//${window.location.hostname}/`); } else { a.classList.add("warning") }
+        if (element.noloader) { a.href=url }
+        else { a.onclick = () => {window.open(`./loader.html?url=${url}&name=${buttonName}`)} }
+        a.textContent = buttonName
+        list.appendChild(a);
     });
 });
